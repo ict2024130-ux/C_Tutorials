f2e60a95bbc4f7cdbcc2a16e6e76e3522b007fc8
ict2024130-ux <251212921+ict2024130-ux@users.noreply.github.com>
2026-07-05 00:58:46 +0530

    new

---
+++ quation01.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void){
+    printf("|===================================================================================|\n");
+    printf("| ==============\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t|\n");
+    printf("||\t\t\t\t|\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t|\n");
+    printf("||\t\t\t\t|\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t|\n");
+    printf("||\t\t\t\t|\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t|\n");
+    printf("||\t\t\t\t|\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t|\n");
+    printf("| ==============\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t|\n");
+    printf("|Name: W.M.Pamodha Vikum Wanasinghe.                                                |\n");
+    printf("|Student ID: ICT/1928/05                                                            |\n");
+    printf("|Email: abc@gmail.com                                                               |\n");
+    printf("|===================================================================================|\n");
+    return 0;
+}
